3a152e981de687083d62c571275c38b8f9439f83
Carl <prinscarl.santoso@mitrais.com>
2016-11-01 16:16:54 +0800

    Add Function 1

--- custom.js
+++ custom.js
@@ -1,6 +1,9 @@
+Ini saya kasih judul
 Ini untuk testing GIT
 Yang akan dilaksanakan 
 di Online Lync
 Pada tanggal 01 November 2016
 dan ini akan diikuti oleh 10 orang peserta
 dari Mitrais
+
+ini ada tambahan dari saya - carl
